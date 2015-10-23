8cd4b0af6fb365da7f0330bd77309e6955354513
Michael Rinaldi <mikerinaldi1@yahoo.com>
2015-10-23 14:40:28 -0400

    Adding the project

---
+++ TreeProgram/TreeProgram/TreeProgram.cpp
@@ -0,0 +1,12 @@
+// TreeProgram.cpp : Defines the entry point for the console application.
+// Mike - The tree is mirrored across the Y axis, from the start position. Exactly. So I decided to only store one half of the tree
+//And when I read it, I just read it in the opposite direction
+
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
